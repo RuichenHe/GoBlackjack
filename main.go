b53371a303ad7567a92524a5a1fd1be7c9e71448
RuichenHe <kylehe970128@gmail.com>
2022-07-13 16:52:09 -0700

    Add a draw function to draw card

--- main.go
+++ main.go
@@ -23,10 +23,14 @@ func main() {
 	var player, dealer Hand
 	for i := 0; i < 3; i++ {
 		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = cards[0], cards[1:]
+			card, cards = draw(cards)
 			*hand = append(*hand, card)
 		}
 	}
 	fmt.Println("Player:", player)
 	fmt.Println("Dealer:", dealer)
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	return cards[0], cards[1:]
 }
