7adf37eefbdf5f2e664e2d9998e49e27c7e8b6a7
RuichenHe <kylehe970128@gmail.com>
2022-07-13 16:47:29 -0700

    Add a player and dealer to draw three cards one by one

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	deck "github.com/RuichenHe/GoCard-module"
 )
 
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
+}
+
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		card, cards = cards[0], cards[1:]
-		fmt.Println(card)
+	var player, dealer Hand
+	for i := 0; i < 3; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = cards[0], cards[1:]
+			*hand = append(*hand, card)
+		}
 	}
+	fmt.Println("Player:", player)
+	fmt.Println("Dealer:", dealer)
 }
