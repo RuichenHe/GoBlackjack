c7e29f344831fcb142bd078518a47b66bfbd6975
RuichenHe <kylehe970128@gmail.com>
2022-07-14 12:57:57 -0700

    Add a dealer AI to control the dealer to hit or stand

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/RuichenHe/GoBlackjack
 
 go 1.18
 
-require github.com/RuichenHe/GoCard-module v0.0.0-20220713232406-ea56fcc6d1f2 // indirect
+require github.com/RuichenHe/GoCard-module v0.0.0-20220713232406-ea56fcc6d1f2
--- main.go
+++ main.go
@@ -16,19 +16,78 @@ func (h Hand) String() string {
 	}
 	return strings.Join(strs, ", ")
 }
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDDEN**"
+}
+func (h Hand) Score() int {
+	score := h.MinScore()
+	if score > 11 {
+		return score
+	}
+	for _, card := range h {
+		if card.Rank == deck.Ace {
+			return score + 10
+		}
+	}
+	return score
+}
+func (h Hand) MinScore() int {
+	score := 0
+	for _, card := range h {
+		score += min(int(card.Rank), 10)
+	}
+	return score
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
 	var player, dealer Hand
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 2; i++ {
 		for _, hand := range []*Hand{&player, &dealer} {
 			card, cards = draw(cards)
 			*hand = append(*hand, card)
 		}
 	}
-	fmt.Println("Player:", player)
-	fmt.Println("Dealer:", dealer)
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("Dealer:", dealer.DealerString())
+		fmt.Println("What will you do? (h)it? (s)stand?")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+	for dealer.Score() < 17 || (dealer.Score() == 17 && dealer.MinScore() < 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
+	pScore, dScore := player.Score(), dealer.Score()
+	fmt.Println("==FINAL CARDS==")
+	fmt.Println("Player:", player, "\nScore:", pScore)
+	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted")
+	case dScore > 21:
+		fmt.Println("Dealer busted")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case pScore < dScore:
+		fmt.Println("You lose...")
+	case pScore == dScore:
+		fmt.Println("Draw.")
+	}
+
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
